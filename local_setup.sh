d4b52041c71e25694b2ef6cbe50d19b1615077db
Sascha Narr <saschanarr@gmail.com>
2021-12-07 14:57:01 +0100

    Add --no-deps to local_setup.sh build commands for easier setup
    Was never required, as the local_install step takes care of dependencies

--- local_setup.sh
+++ local_setup.sh
@@ -1,34 +1,37 @@
 cd cosinnus-core
-python setup.py develop
+python setup.py develop --no-deps
+cd ..
+cd cosinnus-cloud
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-etherpad
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-event
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-message
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-file
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-note
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-notifications
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-marketplace
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-poll
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-stream
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 cd cosinnus-todo
-python setup.py develop
+python setup.py develop --no-deps
 cd ..
 
